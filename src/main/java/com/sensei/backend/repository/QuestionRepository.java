14f2aacdee6f2e754ab1cf30556455afb63587f1
shraddhanemagoudar <123192238+shraddhanemagoudar@users.noreply.github.com>
2025-11-21 14:17:46 +0530

    Added DigitalActivity and question table updates (#12)
    * Added DigitalActivity and question table updates
    
    * Added remaining two files

--- src/main/java/com/sensei/backend/repository/QuestionRepository.java
+++ src/main/java/com/sensei/backend/repository/QuestionRepository.java
@@ -1,9 +1,13 @@
 package com.sensei.backend.repository;
 
-import com.sensei.backend.entity.Questions;
+import com.sensei.backend.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface QuestionsRepository extends JpaRepository<Questions, String> {
+public interface QuestionRepository extends JpaRepository<Question, String> {
+
+    // ✅ Add this method
+    List<Question> findByDigitalActivity_DigitalActivityId(String digitalActivityId);
 }
